f04400d313dea41e35cb224a296a37b9f674e37c
olegomio <84738033+olegomio@users.noreply.github.com>
2024-07-24 14:56:11 +0200

    Create common profiles.lua

---
+++ lua/sftp_plugin/profiles.lua
@@ -0,0 +1,25 @@
+local M = {}
+
+-- Define the base paths as variables
+M.paths = {
+  repo01 = "~/repos/main-page-child-theme", 
+  -- Add more paths as needed
+}
+
+-- Define the profiles with all relevant SFTP information
+M.profiles = {
+  repo01_dev1 = {
+    host = "ip.add.ress",
+    protocol: "sftp",
+    port = 22,
+    secure = true,
+    username = "dev1",
+    remotePath = "/var/www/dev1/wp-content/themes/divi-child",
+    password = "",
+    uploadOnSave = true,
+    localPath = M.paths.repo01
+  }
+  -- Add more profiles as needed
+}
+
+return M
